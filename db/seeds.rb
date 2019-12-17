e5ca832853c91cf39a89ce233d24d94278c22ba9
“Saima <rahman.saima92@gmail.com>
2019-12-16 20:40:02 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Destroy.all
+artist1 = Artist.create(name: "Sonu Nigam")
+artist2 = Artist.create(name: "Lady Gaga")
+artist3 = Artist.create(name: "Arnob")
+
+
+song1 = Song.create(title: "Kal Ho Na Ho", artist_id: artist1.id)
+song2 = Song.create(title: "SHALLOW", artist_id: artist2.id)
+song3 = Song.create(title: "Shei je boshe ache", artist_id: artist3.id)
