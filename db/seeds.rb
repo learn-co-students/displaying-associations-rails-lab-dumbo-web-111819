c4cc79b557e0a23f4125676a8fcbf8e0d94f5f4d
Jonathan Brierre <github email address>
2019-12-17 09:43:14 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Artist.destroy_all
+Song.destroy_all
+
+a1 = Artist.create(name: 'Drake')
+a2 = Artist.create(name: 'Beyonce')
+a3 = Artist.create(name: 'Katy Perry')
+
+s1 = Song.create(title: 'coldline bling', artist_id: a1.id)
+s2 = Song.create(title: 'Holo', artist_id: a2.id)
+s3 = Song.create(title: 'I kissed some chick', artist_id: a3.id)
+s4 = Song.create(title: 'Kiki', artist_id: a1.id)
+s5 = Song.create(title: 'Running', artist_id: a2.id)
+s6 = Song.create(title: 'Some other song', artist_id: a3.id)
